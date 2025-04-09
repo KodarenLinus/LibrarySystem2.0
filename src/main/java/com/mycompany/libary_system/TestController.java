1870be618c8ca6f93fe283fea1949e2ea3f4832f
Linus Sidebäck <linsid-4@student.ltu.se>
2025-04-09 09:51:11 +0200

    Merge origin/master
    # Conflicts:
    #	pom.xml

---
+++ src/main/java/com/mycompany/libary_system/TestController.java
@@ -0,0 +1,29 @@
+package com.mycompany.libary_system;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.TextField;
+
+public class TestController {
+
+     @FXML
+    private TextField ISBN;
+
+    @FXML
+    private TextField Location;
+
+    @FXML
+    private TextField Title;
+   
+    
+    @FXML
+    private ToggleButton addNewBook;
+
+    @FXML
+    void addBook(ActionEvent event) {
+        AddBook addBook = new AddBook();
+        addBook.insertBook(Title.getText(), Location.getText(), ISBN.getText());
+    }
+
+}
