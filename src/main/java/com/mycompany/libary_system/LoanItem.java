b2d41a20e38dea35b85f6e0f6090d606409c47b2
Linus Sidebäck <linsid-4@student.ltu.se>
2025-04-12 20:50:29 +0200

    Merge origin/master
    # Conflicts:
    #	src/main/java/com/mycompany/libary_system/ConnDB.java

--- src/main/java/com/mycompany/libary_system/LoanItem.java
+++ src/main/java/com/mycompany/libary_system/LoanItem.java
@@ -16,20 +16,23 @@ import java.util.ArrayList;
  * @author emildahlback
  */
 public class LoanItem {
-    
+
     public boolean addToLoanRows (int custID, ArrayList<Items> itemsToLoan) {
                 
         // Skapar ett connection objekt ocj spara en SQL query i en String.
         DatabaseConnector connDB = new ConnDB();
         Connection conn = connDB.connect();
+        
+        //SQL-statments
         String sql = "INSERT INTO loan (customerID) VALUES (?)";
         String selectMaxLoanId = "SELECT MAX(loanid) FROM loan WHERE customerID = ?";
+        String selectCategoryId = "SELECT categoryID FROM item WHERE itemID = ?";
         String sql_ = "INSERT INTO loanrow (loanid, itemid, RowLoanStartDate, RowLoanEndDate) VALUES (?, ?, ?, ?)";
         
         LocalDate today = LocalDate.now();
         
         // Add 1 week
-        LocalDate inOneWeeks = today.plusWeeks(1);
+        LocalDate inOneWeek = today.plusWeeks(1);
         
         // Add 2 weeks
         LocalDate inTwoWeeks = today.plusWeeks(2);
@@ -41,33 +44,61 @@ public class LoanItem {
             PreparedStatement stmt = conn.prepareStatement(sql);    
             PreparedStatement getLoanIDStmt = conn.prepareStatement(selectMaxLoanId);
             PreparedStatement stmt1 = conn.prepareStatement(sql_);
+            PreparedStatement getCategoryIDStmt = conn.prepareStatement(selectCategoryId);
         ){
-            
+            // Sätter kund id på loanet
             stmt.setInt(1, custID);
             stmt.executeUpdate();
-
+            
+            // hämtar lånet baserats på kundens senast lån
             getLoanIDStmt.setInt(1, custID);
             ResultSet rs = getLoanIDStmt.executeQuery();
-
+            
+            // Kollar igenom resultsetet
             if (rs.next()) {
+                //Sparar loanID i en lokal variabel
                 int loanID = rs.getInt(1);
-
+                
+                //Lopar igenom alla våra items i våran kundvagn
                 for (Items item : itemsToLoan) {
                     stmt1.setInt(1, loanID);
                     stmt1.setInt(2, item.getItemID());
                     stmt1.setString(3, today.toString()); 
                     
                     //Lägg till if statment som väljer vilket datum saker skall göras.
-                    stmt1.setString(4, nextMonth.toString());
+                    getCategoryIDStmt.setInt(1, item.getItemID());
+                    ResultSet rs1 = getCategoryIDStmt.executeQuery();
+                    if (rs1.next()) {
+                        int categoryID = rs1.getInt("categoryID");
+
+                        // kollar items category och väljer hur länge som man får låna itemet!!
+                        switch (categoryID) {
+                            case 1:
+                                stmt1.setString(4, nextMonth.toString());
+                                break;
+                            case 2:
+                                stmt1.setString(4, inTwoWeeks.toString());
+                                break;
+                            case 3:
+                                stmt1.setString(4, inOneWeek.toString());
+                                break;
+                            default:
+                                stmt1.setString(4, today.toString()); 
+                                break;
+                        }
+                    }
+                            
+                    //spara in våra loanrows
                     stmt1.addBatch();
                 }
-
+                
+                //Skickar in dem i databasen
                 stmt1.executeBatch();
                 return true;
             }
 
         } catch (SQLException e) {
-            e.printStackTrace(); // or use a logger
+            e.printStackTrace(); 
         }
 
         return false;
