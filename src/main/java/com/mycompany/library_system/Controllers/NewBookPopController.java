988580ada33db1c40e02887772bc75c7a2318fe5
Linus Sidebäck <linsid-4@student.ltu.se>
2025-05-03 09:41:56 +0200

    Merge origin/master
    # Conflicts:
    #	.gitignore

--- src/main/java/com/mycompany/library_system/Controllers/NewBookPopController.java
+++ src/main/java/com/mycompany/library_system/Controllers/NewBookPopController.java
@@ -15,7 +15,7 @@ import javafx.stage.Stage;
  */
 public class NewBookPopController {
     
-     @FXML
+    @FXML
     void close(ActionEvent event) {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.close();
--- src/main/java/com/mycompany/library_system/Logic/ReturnLoanItem.java
+++ src/main/java/com/mycompany/library_system/Logic/ReturnLoanItem.java
@@ -13,20 +13,27 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
- *
+ *En klass som hanterar åter lämning av lån.
+ * 
  * @author Linus, Emil, Oliver, Viggo
  */
 public class ReturnLoanItem {
     
+    /**
+     * En metod för att lämna tillbaka items
+     * 
+     * @param loanRows är de lån vi vi skall lämna tillbaka
+     */
     public void returnItem (ArrayList<LoanRow> loanRows) {
         // Skapar en databaskoppling
         DatabaseConnector connDB = new ConnDB();
         Connection conn = connDB.connect();
         
-        // SQL-fråga som lägger till first och lastname i Author tabelen
+        // SQL-fråga som updatera så att de aktiva lånet inte längre blir aktivt
         String updateLoanRow = "UPDATE LoanRow SET ActiveLoan = false " +
                 "WHERE LoanRowID = ?";
         
+        // Updaterar så att itemet blir tillgängligt för lån
         String updateItem = "UPDATE Item SET Available = true " +
                 "WHERE ItemID = ?";
 
@@ -34,6 +41,7 @@ public class ReturnLoanItem {
             PreparedStatement loanRowStmt = conn.prepareStatement(updateLoanRow);  
             PreparedStatement itemStmt = conn.prepareStatement(updateItem);   
         ){
+            // Loppar igenom våran lista med loanRows och lämnar tillbaka varje item.
             for (LoanRow loanRow: loanRows)
             {
                 loanRowStmt.setInt(1, loanRow.getLoanRowID());
